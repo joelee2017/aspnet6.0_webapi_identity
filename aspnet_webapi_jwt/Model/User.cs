d25b3b3d50a0afca5e7a6fb0fdb4fe4506dd3477
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2023-08-23 15:57:23 +0800

    Delete aspnet_webapi_jwt directory

--- aspnet_webapi_jwt/Model/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace aspnet_webapi_jwt.Model
-{
-    public class User
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public List<string> Roles { get; set; }
-    }
-}
