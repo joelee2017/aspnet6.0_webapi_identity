2c3c1c229396badca8706533c14dec6efd8b61b4
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2023-08-23 15:56:34 +0800

    Add files via upload

---
+++ aspnet_webapi_jwt/Helper/IJwtHelper.cs
@@ -0,0 +1,14 @@
+﻿using aspnet_webapi_jwt.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace aspnet_webapi_jwt.Helper
+{
+    public interface IJwtHelper
+    {
+        string GenerateToken(string userName, int expireMinutes = 120);
+        JwtSecurityToken GetToken(List<Claim> authClaims);
+
+        User ValidateToken(string token);
+    }
+}
---
+++ aspnet_webapi_jwt/Model/JwtTokenModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace aspnet_webapi_jwt.Model
+{
+    public class JwtTokenModel
+    {      
+        public string roles { get; set; }
+        public string jti { get; set; }
+        public string iss { get; set; }
+        public string sub { get; set; }
+        public long exp { get; set; }
+        public long nbf { get; set; }
+        public long iat { get; set; }
+
+        [JsonProperty("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")]
+        public string Name { get; set; }
+
+        [JsonProperty("http://schemas.microsoft.com/ws/2008/06/identity/claims/role")]
+        public List<string> Roles { get; set; }
+    }
+}
